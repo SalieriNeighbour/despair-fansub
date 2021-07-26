0f815c27048b6cb9bf5a5875c8f56d742a7dc68d
Salieri <spinasse22@gmail.com>
2021-07-26 13:18:42 -0300

    Changed footer and increased css compatibility

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -5,6 +5,9 @@ const Footer = () => {
         <footer id="footer" className="footer">
             <p>Despair Fansub © 2021</p>
             <p>Site por Salieri</p>
+            <div className="footer-icons">
+                <a href="https://twitter.com/despairfansub"  target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+            </div>
         </footer>
     );
 }
