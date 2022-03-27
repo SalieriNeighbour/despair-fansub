219ebb13aa8edfa05db7ba51bd6a5315ea284a29
unknown <icvaleifes@gmail.com>
2022-03-27 10:15:21 -0300

    Added icon

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,10 +3,11 @@ import React from 'react';
 const Footer = () => {
     return(
         <footer id="footer" className="footer">
-            <p>Despair Fansub © 2022</p>
+            <p>Despair Fansub © 2021-2022</p>
             <p>Site por Salieri</p>
             <div className="footer-icons">
                 <a href="https://twitter.com/despairfansub"  target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+                <a href="https://discord.gg/M9pEhPWWkN"  target="_blank" rel="noopener noreferrer"><i className="fab fa-discord"></i></a>
             </div>
         </footer>
     );
